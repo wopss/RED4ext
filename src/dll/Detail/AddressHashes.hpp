37920109895ab75bedc3069ebcb12e455c6815fb
Octavian Dima <3403191+wopss@users.noreply.github.com>
2025-01-23 14:59:37 +0200

    Update CollectSaveableSystems hash for patch 2.21

--- src/dll/Detail/AddressHashes.hpp
+++ src/dll/Detail/AddressHashes.hpp
@@ -10,7 +10,7 @@ constexpr std::uint32_t CBaseEngine_LoadScripts = 3570081113UL;
 
 constexpr std::uint32_t CGameApplication_AddState = 4223801011UL;
 
-constexpr std::uint32_t GameInstance_CollectSaveableSystems = 3389547420UL;
+constexpr std::uint32_t GameInstance_CollectSaveableSystems = 3230163856ul;
 constexpr std::uint32_t Global_ExecuteProcess = 2203918127UL;
 constexpr std::uint32_t GsmState_SessionActive_ReportErrorCode = 2141394294UL;
 
